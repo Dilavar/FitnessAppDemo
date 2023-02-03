696bc06e2325c1ef15d443f7f2b931414363a3af
DIlavar <dilavarmalek@gmail.com>
2023-02-03 11:19:06 +0530

    Intitial Commit

---
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.fitnessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.EntryPoint
+
+@EntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
